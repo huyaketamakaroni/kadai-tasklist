f7cfc950717ae2da5d94e02e315238ead33840a3
huyaketamakaroni <huyaketamakaroni@gmail.com>
2021-02-17 15:31:06 +0000

    tasklist ver01

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//Route::get('tasks/{id}', 'TasksController@show');
+Route::get('/', 'TasksController@index');
+Route::resource('tasks', 'TasksController');
+
+
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('tasks/{id}', 'TasksController@show');
+Route::post('tasks', 'TasksController@store');
+Route::put('tasks/{id}', 'TasksController@update');
+Route::delete('tasks/{id}', 'TasksController@destroy');
+
+// index: showの補助ページ
+Route::get('/', 'TasksController@index');
+Route::get('tasks/create', 'TasksController@create');
+Route::get('tasks/{id}/edit', 'TasksController@edit')->name('tasks.edit');
+
+*/
